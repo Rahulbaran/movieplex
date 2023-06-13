45a590353bac25dac5d19bd8d98c6b205dbebfc8
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-13 20:54:38 +0530

    ci: use "useFetch" hook to fetch movies data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,21 @@
+import useFetch from "../hooks/useFetch";
+
+import Loader from "../components/Loader";
+
 export default function Home() {
+  const response = useFetch("/.netlify/functions/getMovies");
+
+  if (!response.status) {
+  return <Loader />;
+  } else if (response.status === "fail") {
+    return (
+      <main className="container error-container">
+        <h1>{response.error.message}</h1>
+        <p>Try again after sometimes</p>
+      </main>
+    );
+  }
+
   return (
     <main className="container home-container">
       <h1>Home Container</h1>
