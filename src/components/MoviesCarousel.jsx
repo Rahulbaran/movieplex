df4558acde416253feba62a1edbb762d1e99014c
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-15 17:42:19 +0530

    feat: add content to movies carousel and style it

--- src/components/MoviesCarousel.jsx
+++ src/components/MoviesCarousel.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Pagination, A11y, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -20,13 +21,36 @@ export default function MoviesCarousel({ movies }) {
           <SwiperSlide
             key={movie.id}
             style={{
-              backgroundImage: `linear-gradient(to top, oklch(2% .05 200 / .7), oklch(10% 0.05 200 / .3)), url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+              backgroundImage: `linear-gradient(to top, oklch(2% .05 200 / .85), oklch(10% 0.05 200 / .6)), url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
               backgroundPosition: "center"
             }}
             className="movie-swiper"
-          ></SwiperSlide>
+          >
+            <div className="movie-content">
+              <Link to={`/movie/${movie.id}`}>
+                <h2>{movie.title}</h2>
+              </Link>
+              <p>
+                {movie.overview.length > 300
+                  ? `${movie.overview.slice(0, 300)}...`
+                  : movie.overview}
+              </p>
+              <Link to={`/movie/${movie.id}`}>Know More</Link>
+            </div>
+
+            <div className="movie-image-container">
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                alt={movie.title}
+                loading="lazy"
+                decoding="async"
+                width="350"
+                height="480"
+              />
+            </div>
+          </SwiperSlide>
         );
       })}
     </Swiper>
