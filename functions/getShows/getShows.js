99452817f43afeb827d5198c559774d234fc5374
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-15 19:02:11 +0530

    feat: use netlify function "getShows" to fetch tvshows

--- functions/getShows/getShows.js
+++ functions/getShows/getShows.js
@@ -1,11 +1,33 @@
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+const TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
+
 export const handler = async event => {
+  const type = event.queryStringParameters.type || "popular";
+
   try {
-    const subject = event.queryStringParameters.name || "World";
+    const options = {
+      method: "GET",
+      url: `https://api.themoviedb.org/3/tv/${type}?language=en-US&page=1`,
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`
+      }
+    };
+
+    const response = await axios.request(options);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
+      body: JSON.stringify({ status: "success", response: response.data })
     };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ status: "fail", message: error.toString() })
+    };
   }
 };
