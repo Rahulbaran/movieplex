4722ea287266b0a0901a3a9810cf0fb2fc164e8a
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-15 19:03:18 +0530

    feat: use "useFetch" hook to fetch "shows" & "movies" data using netlify functions
    - fetched "movies" data in "Movies" component
    - fetched "shows" data in "TvShows" component

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,25 @@
+// Hooks
 import useMeta from "../hooks/useMeta";
+import useFetch from "../hooks/useFetch";
+
+// Components
+import Loader from "../components/Loader";
 
 export default function Movies() {
   useMeta({ title: "Movies | Movieplex", description: "" });
+  const res = useFetch("/.netlify/functions/getMovies");
+  
+
+  if (!res.status) {
+    return <Loader />;
+  } else if (res.status === "fail") {
+    return (
+      <main className="container error-container">
+        <h1>{res.error.message}</h1>
+        <p>Try again after sometimes</p>
+      </main>
+    );
+  }
 
   return (
     <section>
--- src/pages/TvShows.jsx
+++ src/pages/TvShows.jsx
@@ -1,7 +1,24 @@
+// Hooks
 import useMeta from "../hooks/useMeta";
+import useFetch from "../hooks/useFetch";
+
+// Components
+import Loader from "../components/Loader";
 
 export default function TvShows() {
   useMeta({ title: "TvShows | Movieplex", description: "" });
+  const res = useFetch("/.netlify/functions/getShows");
+
+  if (!res.status) {
+    return <Loader />;
+  } else if (res.status === "fail") {
+    return (
+      <main className="container error-container">
+        <h1>{res.error.message}</h1>
+        <p>Try again after sometimes</p>
+      </main>
+    );
+  }
 
   return (
     <section>
