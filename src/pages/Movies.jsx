d8b584f090a84d4ed0901b34711741721972aa3c
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-21 15:39:32 +0530

    feat: add load more features in "TvShows" and "Movies" page components
    - removed "useFetch" custom hook
    - used "useEffect" and "useState" for fetching data
    - added load more feature

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,14 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 // Hooks
 import useMeta from "../hooks/useMeta";
-import useFetch from "../hooks/useFetch";
 
 // Components
 import Loader from "../components/Loader";
 
 export default function Movies() {
   useMeta({ title: "Movies | Movieplex", description: "" });
-  const res = useFetch("/.netlify/functions/getMovies");
-  
+
+  const [page, setPage] = useState(1);
+  const [res, setRes] = useState({ status: "", movies: [] });
+  const [initialRender, setInitialRender] = useState(true);
+
+  useEffect(() => {
+    if (initialRender) {
+      setInitialRender(false);
+      return;
+    }
+    const fetchMovies = async () => {
+      try {
+        const response = await axios(
+          `/.netlify/functions/getMovies?page=${page}`
+        );
+        setRes(prev => ({
+          status: "success",
+          movies: [...prev.movies, ...response.data.movies.results]
+        }));
+      } catch (error) {
+        setRes({ status: "fail", error });
+      }
+    };
+    fetchMovies();
+  }, [page, initialRender]);
+
+  const loadMovies = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   if (!res.status) {
     return <Loader />;
@@ -23,7 +52,11 @@ export default function Movies() {
 
   return (
     <section>
-      <h1>Movies Container</h1>
+      <h1>Movies</h1>
+      {res.movies.map(movie => (
+        <p key={movie.id}>{movie.title}</p>
+      ))}
+      <button onClick={loadMovies}>Load more</button>
     </section>
   );
 }
--- src/pages/TvShows.jsx
+++ src/pages/TvShows.jsx
@@ -1,13 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 // Hooks
 import useMeta from "../hooks/useMeta";
-import useFetch from "../hooks/useFetch";
 
 // Components
 import Loader from "../components/Loader";
 
 export default function TvShows() {
   useMeta({ title: "TvShows | Movieplex", description: "" });
-  const res = useFetch("/.netlify/functions/getShows");
+
+  const [page, setPage] = useState(1);
+  const [res, setRes] = useState({ status: "", shows: [] });
+  const [initialRender, setInitialRender] = useState(true);
+
+  useEffect(() => {
+    if (initialRender) {
+      setInitialRender(false);
+      return;
+    }
+    const fetchShows = async () => {
+      try {
+        const response = await axios(
+          `/.netlify/functions/getShows?page=${page}`
+        );
+        setRes(prev => ({
+          status: "success",
+          shows: [...prev.shows, ...response.data.shows.results]
+        }));
+      } catch (error) {
+        setRes({ status: "fail", error });
+      }
+    };
+    fetchShows();
+  }, [page, initialRender]);
+
+  const loadShows = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   if (!res.status) {
     return <Loader />;
@@ -22,7 +52,11 @@ export default function TvShows() {
 
   return (
     <section>
-      <h1>TvShows Container</h1>
+      <h1>Shows</h1>
+      {res.shows.map(show => (
+        <p key={show.id}>{show.name}</p>
+      ))}
+      <button onClick={loadShows}>Load more</button>
     </section>
   );
 }
