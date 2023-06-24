9ef943e1c9d71df9f084595f98add98a4667c479
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-24 07:43:30 +0530

    feat: create header and navigation in both "Movies" & "Shows"
    - added navigation and header section in layouts of "Movies" & "Shows" pages
    - applied styling to both the sections

--- src/layouts/MoviesLayout.jsx
+++ src/layouts/MoviesLayout.jsx
@@ -1,8 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function MoviesLayout() {
   return (
     <main className="container movies-container">
+      <header className="movies-header">
+        <h1>Movies</h1>
+      </header>
+
+      <nav className="movies-navigation">
+        <ul>
+          <li>
+            <NavLink to="/movies/upcoming">Upcoming</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/popular">Popular</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/top_rated">Top Rated</NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <Outlet />
     </main>
   );
--- src/layouts/TvShowsLayout.jsx
+++ src/layouts/TvShowsLayout.jsx
@@ -1,8 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function TvShowsLayout() {
   return (
     <main className="container tvshows-container">
+      <header className="shows-header">
+        <h1>TV Shows</h1>
+      </header>
+
+      <nav className="shows-navigation">
+        <ul>
+          <li>
+            <NavLink to="/tvshows/on_the_air">On The Air</NavLink>
+          </li>
+          <li>
+            <NavLink to="/tvshows/popular">Popular</NavLink>
+          </li>
+          <li>
+            <NavLink to="/tvshows/top_rated">Top Rated</NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <Outlet />
     </main>
   );
