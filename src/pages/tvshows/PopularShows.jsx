9feca8126f0c2fd7286e7cbe24ce4ccdcf257e2a
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-24 13:32:43 +0530

    feat: add the feature of "load more" in "TVShows" page component

--- src/pages/tvshows/PopularShows.jsx
+++ src/pages/tvshows/PopularShows.jsx
@@ -1,3 +1,67 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// Hooks
+import useMeta from "../../hooks/useMeta";
+
+// Components
+import Loader from "../../components/Loader";
+import Card from "../../components/Card";
+
 export default function PopularShows() {
-  return <></>;
+  useMeta({ title: "TVShows | Movieplex", description: "" });
+
+  const [page, setPage] = useState(1);
+  const [res, setRes] = useState({ status: "", shows: [] });
+  const [initialRender, setInitialRender] = useState(true);
+
+  useEffect(() => {
+    if (initialRender) {
+      setInitialRender(false);
+      return;
+    }
+    const fetchShows = async () => {
+      try {
+        const response = await axios(
+          `/.netlify/functions/getShows?page=${page}`
+        );
+        setRes(prev => ({
+          status: "success",
+          shows: [...prev.shows, ...response.data.shows.results]
+        }));
+      } catch (error) {
+        setRes({ status: "fail", error });
+      }
+    };
+    fetchShows();
+  }, [page, initialRender]);
+
+  const loadShows = () => setPage(prevPage => prevPage + 1);
+
+  if (!res.status) {
+    return <Loader />;
+  } else if (res.status === "fail") {
+    return (
+      <section className="container error-container">
+        <h1>{res.error.message}</h1>
+        <p>Try again after sometimes</p>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <section className="cards">
+        {res.shows.map(show => (
+          <Card data={show} key={show.id} />
+        ))}
+      </section>
+
+      <section className="load-more-container">
+        <button className="btn btn-primary" onClick={loadShows}>
+          Load more
+        </button>
+      </section>
+    </>
+  );
 }
--- src/pages/tvshows/TvShows.jsx
+++ src/pages/tvshows/TvShows.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 
 // Hooks
@@ -6,13 +7,31 @@ import useMeta from "../../hooks/useMeta";
 
 // Components
 import Loader from "../../components/Loader";
+import Card from "../../components/Card";
 
-export default function TvShows() {
-  useMeta({ title: "TvShows | Movieplex", description: "" });
+// Utils
+import { modifyTitle } from "../../utils/modifyTitle";
+
+export default function Shows() {
+  const curType = useParams().type;
+
+  useMeta({
+    title: `${modifyTitle(curType)} Shows | Movieplex`,
+    description: ""
+  });
 
   const [page, setPage] = useState(1);
+  const [type, setType] = useState(curType);
   const [res, setRes] = useState({ status: "", shows: [] });
   const [initialRender, setInitialRender] = useState(true);
+
+  useEffect(() => {
+    if (type !== curType) {
+      setType(curType);
+      setRes({ status: "", shows: [] });
+      setPage(1);
+    }
+  }, [type, curType]);
 
   useEffect(() => {
     if (initialRender) {
@@ -22,7 +41,7 @@ export default function TvShows() {
     const fetchShows = async () => {
       try {
         const response = await axios(
-          `/.netlify/functions/getShows?page=${page}`
+          `/.netlify/functions/getShows?page=${page}&type=${type}`
         );
         setRes(prev => ({
           status: "success",
@@ -33,26 +52,34 @@ export default function TvShows() {
       }
     };
     fetchShows();
-  }, [page, initialRender]);
+  }, [page, type, initialRender]);
 
-  const loadShows = () => {
-    setPage(prevPage => prevPage + 1);
-  };
+  const loadShows = () => setPage(prevPage => prevPage + 1);
 
   if (!res.status) {
     return <Loader />;
   } else if (res.status === "fail") {
     return (
-      <main className="container error-container">
+      <section className="container error-container">
         <h1>{res.error.message}</h1>
         <p>Try again after sometimes</p>
-      </main>
+      </section>
     );
   }
 
   return (
     <>
-     
+      <section className="cards">
+        {res.shows.map(show => (
+          <Card data={show} key={show.id} />
+        ))}
+      </section>
+
+      <section className="load-more-container">
+        <button className="btn btn-primary" onClick={loadShows}>
+          Load more
+        </button>
+      </section>
     </>
   );
 }
