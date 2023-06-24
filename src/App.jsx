fdf8ad366fdb9e0eda0635ae2052b1895dd40dc5
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-24 08:04:19 +0530

    ci: change the router layout for "movies" and "tvshows" pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,11 @@ import TvShowsLayout from "./layouts/TvShowsLayout";
 
 /* Pages */
 import Home from "./pages/home/Home";
-import Movies from "./pages/Movies";
+import Movies from "./pages/movies/Movies";
+import PopularMovies from "./pages/movies/PopularMovies";
 import MovieDetails from "./pages/MovieDetails";
-import TvShows from "./pages/TvShows";
+import TvShows from "./pages/tvshows/TvShows";
+import PopularShows from "./pages/tvshows/PopularShows";
 import TvShowDetails from "./pages/TvShowDetails";
 import Search from "./pages/Search";
 import AboutUs from "./pages/AboutUs";
@@ -27,22 +29,23 @@ const router = createBrowserRouter(
       <Route index element={<Home />} />
 
       <Route path="movies" element={<MoviesLayout />}>
-        <Route index element={<Movies />} />
+        <Route index element={<PopularMovies />} />
         <Route
           path=":type"
           element={<Movies />}
           errorElement={<h1>Not Available</h1>}
         />
       </Route>
-      <Route path="movie/:id" element={<MovieDetails />} />
       <Route path="tvshows" element={<TvShowsLayout />}>
-        <Route index element={<TvShows />} />
+        <Route index element={<PopularShows />} />
         <Route
           path=":type"
           element={<TvShows />}
           errorElement={<h1>Not Available</h1>}
         />
       </Route>
+
+      <Route path="movie/:id" element={<MovieDetails />} />
       <Route path="tvshow/:id" element={<TvShowDetails />} />
 
       <Route path="search" element={<Search />} />
