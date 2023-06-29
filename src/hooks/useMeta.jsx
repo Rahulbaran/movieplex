e7e9fe5d1dbc3599ab7d4e723d56abe031a24d97
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-29 10:42:42 +0530

    ci: modify hook to change the meta description text in different pages

--- src/hooks/useMeta.jsx
+++ src/hooks/useMeta.jsx
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 const useMeta = ({ title, description }) => {
   useEffect(() => {
     document.title = title;
-    document.description = description;
+    document
+      .querySelector("meta[name=description]")
+      .setAttribute("content", description);
   }, [title, description]);
 };
 
