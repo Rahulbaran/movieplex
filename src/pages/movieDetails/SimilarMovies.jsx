a9ff7f27d2f81ec47cd94928c42d1b59ab10b4b8
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-01 15:34:56 +0530

    ci: add content to "SimilarMovies" section

--- src/pages/movieDetails/SimilarMovies.jsx
+++ src/pages/movieDetails/SimilarMovies.jsx
@@ -1,3 +1,15 @@
-export default function SimilarMovies() {
-  return <section className="similar-movies-section"></section>;
+import Card from "../../components/Card";
+
+export default function SimilarMovies({ movies }) {
+  return (
+    <section className="movie-section similar-movies-section">
+      <h2>More Like This</h2>
+
+      <div className="similar-movies">
+        {movies.map(movie => (
+          <Card data={movie} key={movie.id} />
+        ))}
+      </div>
+    </section>
+  );
 }
