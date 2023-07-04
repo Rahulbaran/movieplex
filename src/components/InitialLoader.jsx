fa53b0339fc3d62e3e6a309c04b1bbdc9f1da42a
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-04 19:59:29 +0530

    feat: create a component for showing loading bar until response is not fetched

---
+++ src/components/InitialLoader.jsx
@@ -0,0 +1,14 @@
+import Loader from "./Loader";
+
+export default function InitialLoader(res) {
+  if (!res.status) {
+    return <Loader />;
+  } else if (res.status === "fail") {
+    return (
+      <div className="container error-container">
+        <h1>{res.error.message}</h1>
+        <p>Try again after sometimes</p>
+      </div>
+    );
+  }
+}
