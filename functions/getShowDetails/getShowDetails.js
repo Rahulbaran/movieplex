4060383ef7aaa45be5596de110f8fecb327829fb
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-05 06:31:10 +0530

    ci: use "axios" to fetch details of a tvshow from tmdb api

--- functions/getShowDetails/getShowDetails.js
+++ functions/getShowDetails/getShowDetails.js
@@ -5,22 +5,84 @@ dotenv.config();
 
 const TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
 
-export const handler = event => {
+const getShowInfo = async show_id => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/tv/${show_id}?language=en-US`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`
+    }
+  };
+  const response = await axios.request(options);
+  return response.data;
+};
+
+const getShowVideos = async show_id => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/tv/${show_id}/videos?language=en-US`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`
+    }
+  };
+  const response = await axios.request(options);
+  return response.data;
+};
+
+const getShowCasts = async show_id => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/tv/${show_id}/credits?language=en-US`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`
+    }
+  };
+  const response = await axios.request(options);
+  return response.data;
+};
+
+const getSimilarShows = async show_id => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/tv/${show_id}/similar?language=en-US&page=1`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`
+    }
+  };
+  const response = await axios.request(options);
+  return response.data;
+};
+
+export const handler = async event => {
   const show_id = event.queryStringParameters.show_id;
-  console.log(show_id);
 
   try {
+    const response = await Promise.all([
+      getShowInfo(show_id),
+      getShowVideos(show_id),
+      getShowCasts(show_id),
+      getSimilarShows(show_id)
+    ]);
     return {
       statusCode: 200,
       body: JSON.stringify({
-        status: "success"
+        status: "success",
+        showInfo: response[0],
+        videos: response[1].results,
+        casts: response[2].cast,
+        similar_shows: response[3].results
       })
     };
   } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        status: "fail"
+        status: "fail",
+        message: error.toString()
       })
     };
   }
