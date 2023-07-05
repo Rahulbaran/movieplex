e9b4dfbf33868efe48eefdc1c6cda92fb3a08936
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-05 06:36:07 +0530

    ci: access "id" parameter and fetch show details from netlify function
    - used "useParams" to get show_id from url
    - used "useFetch" to fetch show details from netlify function
    - passed the received response as props in all the components
    - lazy loaded the "SimilarShows" component
    - used "InitialLoader" until response is not received

--- src/pages/tvshowDetails/TvShowDetails.jsx
+++ src/pages/tvshowDetails/TvShowDetails.jsx
@@ -1,26 +1,36 @@
+import { lazy, Suspense } from "react";
+import { useParams } from "react-router-dom";
+
+// Custom Hooks
+import useFetch from "../../hooks/useFetch";
+
 // Page Components
 import TvShowPoster from "./TvShowPoster";
 import Casts from "./Casts";
 import MoreDetails from "./MoreDetails";
-import SimilarTvShows from "./SimilarTvShows";
+const SimilarTvShows = lazy(() => import("./SimilarTvShows"));
 
 // Other Components
-
-// Custom Hooks
-import useMeta from "../../hooks/useMeta";
+import Loader from "../../components/Loader";
+import InitialLoader from "../../components/InitialLoader";
 
 export default function TvShowDetails() {
-  useMeta({
-    title: "Random Show | Movieplex",
-    description: "random info"
-  });
+  const { id: show_id } = useParams();
+  const res = useFetch(`/.netlify/functions/getShowDetails?show_id=${show_id}`);
+
+  if (res.status !== "success") return <InitialLoader />;
 
   return (
     <main className="container tvshow-details-container">
-      <TvShowPoster />
-      <Casts />
-      <MoreDetails />
-      <SimilarTvShows />
+      <TvShowPoster tvshow={res} />
+      <Casts casts={res.casts} />
+      <MoreDetails res={res} />
+
+      <Suspense fallback={<Loader />}>
+        {res.similar_shows.length > 0 && (
+          <SimilarTvShows shows={res.similar_shows} />
+        )}
+      </Suspense>
     </main>
   );
 }
