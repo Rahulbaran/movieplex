b528e3072d3a38188b7083ad939f70f440d81137
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-07 07:12:15 +0530

    ci: add navigation and header in Search Layout

--- src/layouts/SearchLayout.jsx
+++ src/layouts/SearchLayout.jsx
@@ -1,8 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function SearchLayout() {
   return (
     <main className="container search-container">
+      <header className="search-header">
+        <h1>Search</h1>
+      </header>
+
+      <nav className="search-navigation">
+        <ul>
+          <li>
+            <NavLink to="/movies">Search Movies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/tvshows">Search TVShows</NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <Outlet />
     </main>
   );
