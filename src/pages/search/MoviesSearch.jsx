04b2fd6296bc6434b0f5206f18924f07e508d4a5
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-07 19:45:50 +0530

    ci: use SearchForm component in Search page

--- src/pages/search/MoviesSearch.jsx
+++ src/pages/search/MoviesSearch.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 import useMeta from "../../hooks/useMeta";
+import SearchForm from "./SearchForm";
 
 export default function MoviesSearch() {
   useMeta({ title: "Search Movies | Movieplex", description: "" });
+  const [searchStr, setSearchStr] = useState("");
+
+  const handleSearchStr = str => setSearchStr(str);
+  const handleSearch = e => {
+    e.preventDefault();
+  };
 
   return (
-    <section className="movies-search-section">
-      <h1>Search Movies</h1>
+    <section className="search-section">
+      <div className="search-form-container">
+        <SearchForm
+          type="movie"
+          searchStr={searchStr}
+          handleSearchStr={handleSearchStr}
+          handleSearch={handleSearch}
+        />
+      </div>
     </section>
   );
 }
--- src/pages/search/TvShowsSearch.jsx
+++ src/pages/search/TvShowsSearch.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 import useMeta from "../../hooks/useMeta";
+import SearchForm from "./SearchForm";
 
 export default function TvShowsSearch() {
   useMeta({ title: "Search TVShows | Movieplex", description: "" });
+  const [searchStr, setSearchStr] = useState("");
+
+  const handleSearchStr = str => setSearchStr(str);
+  const handleSearch = e => {
+    e.preventDefault();
+  };
 
   return (
-    <section className="tvshows-search-section">
-      <h1>Search TVShows</h1>
+    <section className="search-section">
+      <div className="search-form-container">
+        <SearchForm
+          type="tvshow"
+          searchStr={searchStr}
+          handleSearchStr={handleSearchStr}
+          handleSearch={handleSearch}
+        />
+      </div>
     </section>
   );
 }
