b7a3fc57fea15db7a15a7f88954a9f994172340a
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-31 20:36:48 +0530

    feat: add vite configuration for PWA (#2)

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
+import { VitePWA } from "vite-plugin-pwa";
+
+import manifest from "./manifest.json";
 
 export default defineConfig({
-  plugins: [react(), legacy({ targets: ["defaults", "not IE 11"] })],
+  build: { emptyOutDir: true },
+  plugins: [
+    react(),
+    legacy({ targets: ["defaults", "not IE 11"] }),
+    VitePWA({
+      manifest,
+      manifestFilename: "site.webmanifest",
+      includeAssets: ["robots.txt"],
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: [
+          "**/*.{css,html,js}",
+          "**/*.{ico,png,webp,gif,jpeg,avif,svg}"
+        ],
+        sourcemap: true
+      },
+      registerType: "prompt"
+    })
+  ],
 
   server: {
     open: true
   },
   preview: {
-    open: true
+    open: true,
+    port: 5000
   }
 });
